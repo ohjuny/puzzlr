9da7b0d91a88e2da77f2bf7b8ec6e6c7e8439970
ohjun <ohjun@umich.edu>
2018-09-27 16:55:09 -0400

    Added markdown and syntax highlighting! Woop :)

---
+++ puzzles/templatetags/markdownify.py
@@ -0,0 +1,27 @@
+from django import template
+import mistune
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+
+register = template.Library()
+
+class HighlightRenderer(mistune.Renderer):
+    def block_code(self, code, lang):
+        if not lang:
+            return '\n<pre><code>%s</code></pre>\n' % \
+                mistune.escape(code)
+        try:
+            lexer = get_lexer_by_name(lang, stripall=True)
+        except:
+            return '\n<pre><code>%s</code></pre>\n' % \
+                mistune.escape(code)
+
+        formatter = HtmlFormatter(linenos='table')
+        return highlight(code, lexer, formatter)
+
+@register.filter
+def markdown(value):
+    renderer = HighlightRenderer()
+    markdown = mistune.Markdown(renderer=renderer)
+    return markdown(value)
